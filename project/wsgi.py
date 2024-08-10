aa857594071b19a9271c06efec68cbbec86be724
maya0513 <ryutaueyama200304@gmail.com>
2024-08-10 12:07:05 +0900

    add post

--- project/wsgi.py
+++ project/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+# 環境変数からポートを取得
+port = int(os.environ.get("PORT", 8000))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
